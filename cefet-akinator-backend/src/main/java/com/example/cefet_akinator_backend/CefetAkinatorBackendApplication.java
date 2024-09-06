0ee105ea514fb37e73f457b6b8aa49bb896485e5
Joao <jpmaresp@gmail.com>
2024-09-06 19:07:17 -0300

    project builded

---
+++ cefet-akinator-backend/src/main/java/com/example/cefet_akinator_backend/CefetAkinatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cefet_akinator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CefetAkinatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CefetAkinatorBackendApplication.class, args);
+	}
+
+}
